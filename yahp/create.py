cd1d8e48b40b6e3b33ce76a36b1b9fb989bf444b
Ravi Rahman <ravi@mosaicml.com>
2021-09-22 12:07:44 -0700

    Restructuring

--- yahp/create.py
+++ yahp/create.py
@@ -35,77 +35,8 @@ def _create_from_dict(cls: Type[hp.Hparams], data: Dict[str, JSON], prefix: List
             # Unwrap typing
             ftype_origin = ftype if type_helpers.get_origin(ftype) is None else type_helpers.get_origin(ftype)
 
-            if f.name in cls.hparams_registry:
-                # Found in registry to unwrap custom Hparams subclasses
-                registry_items = cls.hparams_registry[f.name]
-                sub_data = data[f.name]
-
-                if type_helpers._is_list(ftype_origin):
-                    # parse by key
-                    if sub_data is None:
-                        sub_data = []
-                    parsed_values: List[hp.Hparams] = []
-                    if not isinstance(sub_data, list):
-                        raise ValueError(f"{' '.join(field_prefix)} must be a list")
-                    for item in sub_data:
-                        if not isinstance(item, dict):
-                            raise ValueError(f"{' '.join(field_prefix)} must be a dict")
-                        if not len(item) == 1:
-                            raise ValueError(f"{'.'.join(field_prefix)} must have just one element")
-                        yaml_key, key_data = list(item.items())[0]
-                        sub_prefix = field_prefix + [yaml_key]
-                        if key_data is None:
-                            key_data = {}
-                        if not isinstance(key_data, dict):
-                            raise ValueError(f"{'.'.join(sub_prefix)} must be a dict")
-                        parsed_values.append(
-                            _create_from_dict(
-                                cls=registry_items[yaml_key],
-                                data=key_data,
-                                prefix=sub_prefix,
-                            ))
-                    kwargs[f.name] = parsed_values
-                else:
-                    # Direct Nesting
-                    if sub_data is None:
-                        sub_data = {}
-                    if not isinstance(sub_data, dict):
-                        raise ValueError(f"{' '.join(field_prefix)} must be a dict")
-                    if not len(sub_data) == 1:
-                        raise ValueError(f"{'.'.join(field_prefix)} must have just one element")
-                    yaml_key, key_data = list(sub_data.items())[0]
-                    if yaml_key not in registry_items:
-                        raise ValueError(
-                            f"Unknown key {yaml_key}. Options for {field_prefix} are: {', '.join(registry_items.keys())}."
-                        )
-                    sub_prefix = field_prefix + [yaml_key]
-                    if key_data is None:
-                        key_data = {}
-                    if not isinstance(key_data, dict):
-                        raise ValueError(f"{'.'.join(sub_prefix)} must be a dict")
-                    kwargs[f.name] = _create_from_dict(
-                        cls=registry_items[yaml_key],
-                        data=key_data,
-                        prefix=sub_prefix,
-                    )
-            elif type_helpers._is_hparams_type(type_helpers._get_real_ftype(ftype)):
-                # Must be a directly nested Hparams or Optional[Hparams]
-                if data[f.name] is None:
-                    if type_helpers._is_optional(ftype):
-                        kwargs[f.name] = None
-                        continue
-                    else:
-                        data[f.name] = {}
-                assert isinstance(data[f.name], dict)
-                hparams_cls = type_helpers._get_real_ftype(ftype)
-                assert issubclass(hparams_cls, hp.Hparams)
-                sub_hparams = _create_from_dict(
-                    cls=hparams_cls,
-                    data=cast(Dict[str, JSON], data[f.name]),
-                    prefix=field_prefix,
-                )
-                kwargs[f.name] = sub_hparams
-            else:
+            if not (type_helpers._is_hparams_type(type_helpers._get_real_ftype(ftype)) or
+                    f.name in cls.hparams_registry):
                 # It's a primitive type
                 if type_helpers._is_optional(ftype):
                     if data[f.name] is None:
@@ -113,4 +44,77 @@ def _create_from_dict(cls: Type[hp.Hparams], data: Dict[str, JSON], prefix: List
                     else:
                         ftype = type_helpers._get_real_ftype(ftype)
                 kwargs[f.name] = type_helpers.parse_json_value(data[f.name], ftype, f"{cls.__name__}.{f.name}")
+            else:
+                if f.name not in cls.hparams_registry:
+                    assert type_helpers._is_hparams_type(type_helpers._get_real_ftype(ftype))
+                    # Must be a directly nested Hparams or Optional[Hparams]
+                    if data[f.name] is None:
+                        if type_helpers._is_optional(ftype):
+                            kwargs[f.name] = None
+                            continue
+                        else:
+                            data[f.name] = {}
+                    assert isinstance(data[f.name], dict)
+                    hparams_cls = type_helpers._get_real_ftype(ftype)
+                    assert issubclass(hparams_cls, hp.Hparams)
+                    sub_hparams = _create_from_dict(
+                        cls=hparams_cls,
+                        data=cast(Dict[str, JSON], data[f.name]),
+                        prefix=field_prefix,
+                    )
+                    kwargs[f.name] = sub_hparams
+                else:
+                    # Found in registry to unwrap custom Hparams subclasses
+                    registry_items = cls.hparams_registry[f.name]
+                    sub_data = data[f.name]
+
+                    if type_helpers._is_list(ftype_origin):
+                        # parse by key
+                        if sub_data is None:
+                            sub_data = []
+                        parsed_values: List[hp.Hparams] = []
+                        if not isinstance(sub_data, list):
+                            raise ValueError(f"{' '.join(field_prefix)} must be a list")
+                        for item in sub_data:
+                            if not isinstance(item, dict):
+                                raise ValueError(f"{' '.join(field_prefix)} must be a dict")
+                            if not len(item) == 1:
+                                raise ValueError(f"{'.'.join(field_prefix)} must have just one element")
+                            yaml_key, key_data = list(item.items())[0]
+                            sub_prefix = field_prefix + [yaml_key]
+                            if key_data is None:
+                                key_data = {}
+                            if not isinstance(key_data, dict):
+                                raise ValueError(f"{'.'.join(sub_prefix)} must be a dict")
+                            parsed_values.append(
+                                _create_from_dict(
+                                    cls=registry_items[yaml_key],
+                                    data=key_data,
+                                    prefix=sub_prefix,
+                                ))
+                        kwargs[f.name] = parsed_values
+                    else:
+                        # Direct Nesting
+                        if sub_data is None:
+                            sub_data = {}
+                        if not isinstance(sub_data, dict):
+                            raise ValueError(f"{' '.join(field_prefix)} must be a dict")
+                        if not len(sub_data) == 1:
+                            raise ValueError(f"{'.'.join(field_prefix)} must have just one element")
+                        yaml_key, key_data = list(sub_data.items())[0]
+                        if yaml_key not in registry_items:
+                            raise ValueError(
+                                f"Unknown key {yaml_key}. Options for {field_prefix} are: {', '.join(registry_items.keys())}."
+                            )
+                        sub_prefix = field_prefix + [yaml_key]
+                        if key_data is None:
+                            key_data = {}
+                        if not isinstance(key_data, dict):
+                            raise ValueError(f"{'.'.join(sub_prefix)} must be a dict")
+                        kwargs[f.name] = _create_from_dict(
+                            cls=registry_items[yaml_key],
+                            data=key_data,
+                            prefix=sub_prefix,
+                        )                   
+
     return cls(**kwargs)
