bdbdff97ebb61f45f7417427211510f537122d66
Jamie Bloxham <jamie@mosaicml.com>
2021-09-28 00:49:50 +0000

    lint

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -6,12 +6,12 @@ from tests.yahp_fixtures import OptionalBooleansHparam, YamlInput
 def test_boolean_overrides_explicit(empty_object_yaml_input: YamlInput):
     o = OptionalBooleansHparam.create(filepath=empty_object_yaml_input.filename,
                                       args=['--default_true', 'false', '--default_false', 'true'])
-    assert o.default_true == False
-    assert o.default_false == True
+    assert o.default_true == False  # type: ignore
+    assert o.default_false == True  # type: ignore
 
 
 def test_boolean_overrides_implicit(empty_object_yaml_input: YamlInput):
     o = OptionalBooleansHparam.create(filepath=empty_object_yaml_input.filename,
                                       args=['--default_true', '--default_false'])
-    assert o.default_true == True
-    assert o.default_false == True
+    assert o.default_true == True  # type: ignore
+    assert o.default_false == True  # type: ignore
