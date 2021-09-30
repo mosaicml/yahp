0265bf85c539986f61ff7eb34d2c1f3ca6493a29
Avery Lamp <averylamp@gmail.com>
2021-09-30 08:36:22 -0700

    Bumps version to 0.0.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)
 
 setup(
     name="yahp",
-    version="0.0.5",
+    version="0.0.6",
     author="MosaicML",
     author_email="team@mosaicml.com",
     description="Yet Another HyperParameter framework",
