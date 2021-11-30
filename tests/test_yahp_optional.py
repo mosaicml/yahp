d28d7a57509748116553b80314f8ebd245132ba2
Avery Lamp <averylamp@gmail.com>
2021-11-30 14:57:42 -0800

    Revert "Fix required fields within optional hparams (#82)" (#83)
    This reverts commit c16237ea5eb13b3b593d779ad25c429eab78b0c4.

--- tests/test_yahp_optional.py
+++ tests/test_yahp_optional.py
@@ -1,9 +1,6 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
-import pytest
-
-from tests.yahp_fixtures import (OptionalBooleansHparam, OptionalFieldHparam, OptionalHparamsField,
-                                 OptionalRequiredParentHparam, YamlInput)
+from tests.yahp_fixtures import OptionalBooleansHparam, OptionalFieldHparam, OptionalHparamsField, YamlInput
 
 
 def test_empty_object_optional_field_hparams_data(optional_field_empty_object_yaml_input: YamlInput):
@@ -41,20 +38,3 @@ def test_optional_booleans_hparams_file(empty_object_yaml_input: YamlInput):
     o = OptionalBooleansHparam.create(f=empty_object_yaml_input.filename)
     assert o.default_true
     assert not o.default_false
-
-
-def test_optional_required_missing_optional_hparams_data(optional_required_missing_optional_yaml_input: YamlInput):
-    o = OptionalRequiredParentHparam.create(data=optional_required_missing_optional_yaml_input.dict_data)
-    assert o.optional_child is None
-
-
-def test_optional_required_missing_required_hparams_data(optional_required_missing_required_yaml_input: YamlInput):
-    with pytest.raises(ValueError):
-        OptionalRequiredParentHparam.create(data=optional_required_missing_required_yaml_input.dict_data)
-    # assert o.optional_child is None
-
-
-def test_optional_required_hparams_data(optional_required_yaml_input: YamlInput):
-    o = OptionalRequiredParentHparam.create(data=optional_required_yaml_input.dict_data)
-    assert o.optional_child is not None
-    assert o.optional_child.required_field == 5
