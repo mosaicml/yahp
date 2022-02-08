0ed7d79be1e2e54a478d4127f0acebb32379cf7f
Mihir Patel <mihir.v.patel7@gmail.com>
2022-02-08 15:04:46 -0800

    add requirement to workflow

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ extra_deps["dev"] = {
     'junitparser>=2.1.1',
     'coverage[toml]>=5.5',
     'pytest>=6.2.0',
+    'toml>=0.10.2',
     'yapf>=0.13.0',
     'isort>=5.9.3',
     'bump2version>=1.0.1',
