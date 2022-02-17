d9005cfb99bd8d94e51fe5f258e4b5c2381eb62d
ravi-mosaicml <ravi@mosaicml.com>
2022-02-17 08:58:34 -0800

    Conda configuration + other cleanup (#108)
    1. Added anaconda configuration
    2. Removed the `yahp-inherits` CLI script since a) nothing is using it, and b) didn't want to deal with adding an anaconda entry point.
    3. Added `py.typed` to indicate that yahp has python typing annotations

--- setup.py
+++ setup.py
@@ -45,13 +45,13 @@ setup(
     description="Yet Another HyperParameter framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mosaicml/hparams",
+    url="https://github.com/mosaicml/yahp",
     packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    entry_points={
-        'console_scripts': ['yahp-inherits = yahp.inheritance:main',],
+    package_data={
+        'yahp': ['py.typed'],
     },
     install_requires=install_requires,
     extras_require=extra_deps,
