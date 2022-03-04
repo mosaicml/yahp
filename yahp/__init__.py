adc17dc9614925b4aaa0253dfa66a7739a41dfdb
Avery Lamp <averylamp@gmail.com>
2022-03-04 15:20:05 -0800

    isort

--- yahp/__init__.py
+++ yahp/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 MosaicML. All Rights Reserved.
 
-from .version import __version__
-
 from yahp.field import optional as optional
 from yahp.field import required as required
 from yahp.hparams import Hparams as Hparams
+
+from .version import __version__
