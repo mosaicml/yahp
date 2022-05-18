8d6641c3ae2d2b3c3929bb829eeace20173c8d9d
Ravi Rahman <ravi@mosaicml.com>
2022-05-18 16:13:45 -0700

    Use `__name__` if possible

--- yahp/utils/type_helpers.py
+++ yahp/utils/type_helpers.py
@@ -343,7 +343,7 @@ class HparamsType:
             ans = 'JSON'
 
         else:
-            ans = ', '.join(str(t) for t in self.types)
+            ans = ', '.join(getattr(t, '__name__', str(t)) for t in self.types)
 
         if ans is None:
             # always None
