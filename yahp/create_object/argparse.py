0bc1c999679719c7098988b8d07ce62f2e0ac8c2
Mihir Patel <mihir.v.patel7@gmail.com>
2022-07-28 12:33:03 -0600

    Revert "JSON Schema Support (#123)" (#128)
    This reverts commit 8d1880e9825c2e6a1fc4f574be421cdd4a620539.

--- yahp/create_object/argparse.py
+++ yahp/create_object/argparse.py
@@ -117,10 +117,10 @@ def get_hparams_file_from_cli(
     cli_args: List[str],
     argparse_name_registry: ArgparseNameRegistry,
     argument_parsers: List[argparse.ArgumentParser],
-) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+) -> Tuple[Optional[str], Optional[str]]:
     parser = argparse.ArgumentParser(add_help=False)
     argument_parsers.append(parser)
-    argparse_name_registry.reserve('f', 'file', 'd', 'dump', 'validate')
+    argparse_name_registry.reserve('f', 'file', 'd', 'dump')
     parser.add_argument('-f',
                         '--file',
                         type=str,
@@ -139,13 +139,8 @@ def get_hparams_file_from_cli(
         metavar='stdout',
         help='Dump the resulting Hparams to the specified YAML file (defaults to `stdout`) and exit.',
     )
-    parser.add_argument(
-        '--validate',
-        action='store_true',
-        help='Whether to validate YAML against Hparams.',
-    )
     parsed_args, cli_args[:] = parser.parse_known_args(cli_args)
-    return parsed_args.file, parsed_args.dump, parsed_args.validate
+    return parsed_args.file, parsed_args.dump
 
 
 def get_commented_map_options_from_cli(
